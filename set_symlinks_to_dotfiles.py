628d8999f19c38c2ed707fddf3dbf0015d06aeef
Keisuke Takahashi <keithseahus@gmail.com>
2013-08-26 16:05:40 +0900

    Add a script for setting symlinks to the dotfiles.

---
+++ set_symlinks_to_dotfiles.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+
+files_all = os.listdir('.')
+files_exclude = ['.git', 'README.md', 'set_symlinks_to_dotfiles.py']
+files = list(set(files_all) - set(files_exclude))
+
+homedir = os.getenv("HOME")
+files_homedir = os.listdir(homedir)
+
+for file in files:
+  target = homedir + "/" + file
+
+  if file in files_homedir:
+    file_to_remove = os.path.abspath(target)
+    print "WARNING: %s is already in %s, so remove %s" % (file, homedir, file_to_remove)
+    os.remove(file_to_remove)
+  else:
+    print "INFO: %s is NOT in %s" % (file, homedir)
+
+  print "INFO: create symlink: source = %s, link_name = %s" % (os.path.abspath(file), target)
+  os.symlink(os.path.abspath(file), target)
+
