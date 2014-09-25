305b6faa03c735233683bee9e197b73e0cd99b0e
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-09-25 22:54:18 +0900

    Update elixir-mode

--- .emacs.d/elisp/elixir-mode.el
+++ .emacs.d/elisp/elixir-mode.el
@@ -1,4 +1,4 @@
-;;; elixir-mode.el --- Major mode for editing Elixir files
+;;; elixir-mode.el -- Major mode for editing Elixir files
 
 ;; Copyright (c) 2011 secondplanet
 ;; Author: Humza Yaqoob
@@ -42,28 +42,28 @@
   "Elixir mode keymap.")
 
 (defgroup elixir nil
-        "Elixir major mode."
-        :group 'languages)
+	"Elixir major mode."
+	:group 'languages)
 
 (defcustom elixir-compiler-command "elixirc"
-        "Elixir mode command to compile code. Must be in your path."
-        :type 'string
-        :group 'elixir)
+	"Elixir mode command to compile code. Must be in your path."
+	:type 'string
+	:group 'elixir)
 
 (defcustom elixir-iex-command "iex"
-        "Elixir mode command for interactive REPL. Must be in your path."
-        :type 'string
-        :group 'elixir)
+	"Elixir mode command for interactive REPL. Must be in your path."
+	:type 'string
+	:group 'elixir)
 
 (defcustom elixir-mode-highlight-operators t
-        "Elixir mode option for whether or not to highlight operators."
-        :type 'boolean
-        :group 'elixir)
+	"Elixir mode option for whether or not to highlight operators."
+	:type 'boolean
+	:group 'elixir)
 
 (defcustom elixir-mode-cygwin-paths t
-        "Elixir mode use Cygwin style paths on Windows operating systems."
-        :type 'boolean
-        :group 'elixir)
+	"Elixir mode use Cygwin style paths on Windows operating systems."
+	:type 'boolean
+	:group 'elixir)
 
 (defcustom elixir-mode-cygwin-prefix "/cygdrive/C"
 	"Elixir mode Cygwin prefix."
@@ -74,19 +74,6 @@
   (if elixir-mode-debug-flag
       (message s objects)))
 
-(defvar elixir-mode-define-names '(
-  "def"
-  "defdelegate"
-  "defimpl"
-  "defmacro"
-  "defmacrop"
-  "defmodule"
-  "defoverridable"
-  "defp"
-  "defprotocol"
-  "defrecord"
-  "destructure")
-"Elixir mode def-like keywords.")
 (defvar elixir-mode-keyword-names '(
   "->"
   "bc"
@@ -131,11 +118,6 @@
   "catch"
   "rescue"
   "fn"
-  "function"
-  "%r"
-  "%R"
-  "%b"
-  "%B"
   "receive"
   "end")
 "Elixir mode keywords.")
@@ -226,19 +208,27 @@
 	"For use with operators."
 	:group 'font-lock-faces)
 
+(defvar font-lock-atom-face 'font-lock-atom-face)
+(defface font-lock-operator-face
+	'((((type tty) (class color)) nil)
+		(((class color) (background light))
+		(:foreground "magenta"))
+	(t nil))
+	"For use with atoms."
+	:group 'font-lock-faces)
+
 (defconst elixir-mode-font-lock-defaults
   (list
     '("#.*$" . font-lock-comment-face)                                                                                                  ; comments
-    `(,(concat "^\\s *\\<" (regexp-opt elixir-mode-define-names t) "\\>\\s +\\([^( \t\n]+\\)") 2 font-lock-function-name-face)          ; methods
+    '("^\\s *defp?\\s +\\([^( \t\n]+\\)" . font-lock-function-name-face)                                                                  ; methods
     `(,(concat "\\<" (regexp-opt elixir-mode-keyword-names t) "\\>") . font-lock-keyword-face)                                          ; keywords
     `(,(concat "\\<" (regexp-opt elixir-mode-builtin-names t) "\\>") . font-lock-builtin-face)                                          ; builtins
     `(,(concat "\\<" (regexp-opt elixir-mode-module-names t) "\\>") . font-lock-type-face)                                              ; core modules
     (when elixir-mode-highlight-operators `(,(concat "\\<" (regexp-opt elixir-mode-operator-names t) "\\>") . font-lock-operator-face)) ; operators
     '("\\(\\w*\\)\\s-*:?=" . font-lock-variable-name-face)                                                                              ; variables
     '("-[Rr].*[ \n\t]" . font-lock-constant-face)                                                                                       ; regexes
-    '("\\<\\(true\\|false\\|nil\\)\\>" . font-lock-reference-face)                                                                           ; atoms, boolean
-    '("\\w*:\s" . font-lock-reference-face)
-    '(":\\w*" . font-lock-reference-face))                                                                                                   ; atoms, generic
+    '("\\<\\(true\\|false\\|nil\\)\\>" . font-lock-atom-face)                                                                           ; atoms, boolean
+    '("'\\w*" . font-lock-atom-face))                                                                                                   ; atoms, generic
 "Highlighting for Elixir mode.")
 
 (defun elixir-mode-takewhile (f l)
@@ -275,8 +265,8 @@
 (defun elixir-mode-find-type ()
   "find keyword type in (begin|end|normal|label|key)"
   (progn
-    (elixir-mode-message "%s" "find-type")
-    (elixir-mode-message "%s" (thing-at-point 'line))
+    (elixir-mode-message "find-type")
+    (elixir-mode-message (thing-at-point 'line))
     (cond ((looking-at elixir-mode-endmark)
 	   'end)
 	  ((looking-at elixir-mode-beginmark)
@@ -326,60 +316,59 @@
 			      elixir-basic-offset))
 			  ((or (eq ttype 'normal) (eq ttype 'begin))
 			   (elixir-mode-previous-line-offset)))))
-    (cond ((> 0 noffset) 
-	   (setq noffset 0)))
     (indent-line-to noffset)))
+
 (defun elixir-mode-cygwin-path (expanded-file-name)
-        "Elixir mode get Cygwin absolute path name."
-        (replace-regexp-in-string "^[a-zA-Z]:" elixir-mode-cygwin-prefix expanded-file-name t))
+	"Elixir mode get Cygwin absolute path name."
+	(replace-regexp-in-string "^[a-zA-Z]:" elixir-mode-cygwin-prefix expanded-file-name t))
 
 (defun elixir-mode-universal-path (file-name)
-        "Elixir mode multi-OS path handler."
-        (let ((full-file-name (expand-file-name file-name)))
-                (if (and (equal system-type 'windows-nt)
-                                                 elixir-mode-cygwin-paths)
-                                (elixir-mode-cygwin-path full-file-name)
-                                full-file-name)))
+	"Elixir mode multi-OS path handler."
+	(let ((full-file-name (expand-file-name file-name)))
+		(if (and (equal system-type 'windows-nt)
+						 elixir-mode-cygwin-paths)
+				(elixir-mode-cygwin-path full-file-name)
+				full-file-name)))
 
 (defun elixir-mode-command-compile (file-name)
   "Elixir mode command to compile a file."
-        (let ((full-file-name (elixir-mode-universal-path file-name)))
-                (mapconcat 'identity (append (list elixir-compiler-command) (list full-file-name)) " ")))
+	(let ((full-file-name (elixir-mode-universal-path file-name)))
+		(mapconcat 'identity (append (list elixir-compiler-command) (list full-file-name)) " ")))
 
 (defun elixir-mode-compiled-file-name (&optional filename)
   "Elixir mode compiled filename."
-        (concat (file-name-sans-extension (or filename (buffer-file-name))) ".beam"))
+	(concat (file-name-sans-extension (or filename (buffer-file-name))) ".beam"))
 
 (defun elixir-mode-compile-file ()
   "Elixir mode compile and save current file."
   (interactive)
-        (let ((compiler-output (shell-command-to-string (elixir-mode-command-compile (buffer-file-name)))))
-                (when (string= compiler-output "")
-                        (message "Compiled and saved as %s" (elixir-mode-compiled-file-name)))))
+	(let ((compiler-output (shell-command-to-string (elixir-mode-command-compile (buffer-file-name)))))
+		(when (string= compiler-output "")
+			(message "Compiled and saved as %s" (elixir-mode-compiled-file-name)))))
 
 (defun elixir-mode-iex ()
   "Elixir mode interactive REPL."
-        (interactive)
-        (unless (comint-check-proc "*IEX*")
-                (set-buffer
-                 (apply 'make-comint "IEX"
+	(interactive)
+	(unless (comint-check-proc "*IEX*")
+		(set-buffer
+		 (apply 'make-comint "IEX"
             elixir-iex-command nil '())))
-        (pop-to-buffer "*IEX*"))
+	(pop-to-buffer "*IEX*"))
 
 (defun elixir-mode-open-modegithub ()
   "Elixir mode open GitHub page."
   (interactive)
-        (browse-url "https://github.com/secondplanet/elixir-mode"))
+	(browse-url "https://github.com/secondplanet/elixir-mode"))
 
 (defun elixir-mode-open-elixir-home ()
   "Elixir mode go to language home."
   (interactive)
-        (browse-url "https://github.com/josevalim/elixir#README"))
+	(browse-url "https://github.com/josevalim/elixir#README"))
 
 (defun elixir-mode-show-version ()
   "Elixir mode print version."
 	(interactive)
-	(message "%s" (concat "elixir-mode v" elixir-mode-version " " elixir-mode-date " by Humza Yaqoob")))
+	(message (concat "elixir-mode v" elixir-mode-version " " elixir-mode-date " by Humza Yaqoob")))
 
 (defvar elixir-mode-syntax-table
   (let ((elixir-mode-syntax-table (make-syntax-table)))
@@ -393,48 +382,40 @@
     (modify-syntax-entry ?\} "){" elixir-mode-syntax-table)
     (modify-syntax-entry ?\[ "(]" elixir-mode-syntax-table)
     (modify-syntax-entry ?\] ")[" elixir-mode-syntax-table)
-    (modify-syntax-entry ?\: "'" elixir-mode-syntax-table)
     elixir-mode-syntax-table)
 "Elixir mode syntax table.")
 
 (easy-menu-define elixir-mode-menu elixir-mode-map
   "Elixir mode menu."
   '("Elixir"
-    ["Indent line" smie-indent-line]
-    ["Compile file" elixir-mode-compile-file]
-    ["IEX" elixir-mode-iex]
-    "---"
-    ["elixir-mode on GitHub" elixir-mode-open-modegithub]
-    ["Elixir homepage" elixir-mode-open-elixirhome]
+    ["Indent line" elixir-mode-indent-line]
+		["Compile file" elixir-mode-compile-file]
+		["IEX" elixir-mode-iex]
+		"---"
+		["elixir-mode on GitHub" elixir-mode-open-modegithub]
+		["Elixir homepage" elixir-mode-open-elixirhome]
     ["About" elixir-mode-show-version]
-    ))
+  ))
 
 (defun elixir-mode ()
   "Major mode for editing Elixir files."
-  (interactive)
-  (kill-all-local-variables)
-  (set-syntax-table elixir-mode-syntax-table)
-  (set (make-local-variable 'font-lock-defaults) '(elixir-mode-font-lock-defaults))
-  (setq major-mode 'elixir-mode)
-  (setq mode-name "Elixir")
-  (set (make-local-variable 'comment-start) "# ")
-  (set (make-local-variable 'comment-end) "")
-  (smie-setup elixir-smie-grammar 'verbose-elixir-smie-rules ; 'elixir-smie-rules
-              :forward-token 'elixir-smie-forward-token
-              :backward-token 'elixir-smie-backward-token)
-  (run-hooks 'elixir-mode-hook)
-  (run-hooks 'prog-mode-hook))
+    (interactive)
+    (kill-all-local-variables)
+    (set-syntax-table elixir-mode-syntax-table)
+    (set (make-local-variable 'indent-line-function) 'elixir-mode-indent-line)
+    (set (make-local-variable 'font-lock-defaults) '(elixir-mode-font-lock-defaults))
+    (setq major-mode 'elixir-mode)
+    (setq mode-name "Elixir")
+    (run-hooks 'elixir-mode-hook))
 
 (define-minor-mode elixir-cos-mode
-        "Elixir mode toggle compile on save."
-        :group 'elixir-cos :lighter " CoS"
-        (cond
-         (elixir-cos-mode
-                (add-hook 'after-save-hook 'elixir-mode-compile-file nil t))
-         (t
-                (remove-hook 'after-save-hook 'elixir-mode-compile-file t))))
-
-(require 'elixir-smie)
+	"Elixir mode toggle compile on save."
+	:group 'elixir-cos :lighter " CoS"
+	(cond
+	 (elixir-cos-mode
+		(add-hook 'after-save-hook 'elixir-mode-compile-file nil t))
+	 (t
+		(remove-hook 'after-save-hook 'elixir-mode-compile-file t))))
 
 (provide 'elixir-mode)
 
@@ -443,6 +424,5 @@
 (add-to-list 'auto-mode-alist '("\\.ex\\'" . elixir-mode))
 ;;;###autoload
 (add-to-list 'auto-mode-alist '("\\.exs\\'" . elixir-mode))
-
 
 ;;; elixir-mode.el ends here
